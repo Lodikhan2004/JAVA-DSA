ab0f4ad6d00e68416375d0cd638f1b8cec1260a5
Lodikhan2004 <lodikhan2004@gmail.com>
2024-11-09 20:10:31 +0530

    Added new logic to InvertedHP

--- Patterns/InvertedHalfPyramid.java
+++ Patterns/InvertedHalfPyramid.java
@@ -6,8 +6,8 @@ public class InvertedHalfPyramid {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=n;j>=i;j--){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
