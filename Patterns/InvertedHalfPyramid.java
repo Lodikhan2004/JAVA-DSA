470dc9d663d54a303b679e67a0670c3b110c8457
Lodikhan2004 <lodikhan2004@gmail.com>
2024-11-09 20:03:44 +0530

    Added InvertedHalfPyraid

---
+++ Patterns/InvertedHalfPyramid.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.*;
+public class InvertedHalfPyramid {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+
