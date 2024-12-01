7c7f92ba6789c6cb963ab376248fd24a33319158
Lodikhan2004 <lodikhan2004@gmail.com>
2024-12-02 00:16:44 +0530

    new

--- Assignment/array2d2.java
+++ Assignment/array2d2.java
@@ -2,6 +2,7 @@ package Assignment;
 
 public class array2d2 {
     public static void main(String[] args) {
+        //Print out the sum of the numbers inthe second row of the “nums” array
         int Sum=0;
         int[][] array = { {1,4,9},{11,4,3},{2,2,3} };
          /*for(int i=0; i<array.length; i++){
