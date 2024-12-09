4f8171269c2c8ac37aa9df9660f2ffb00ae71a96
Lodikhan2004 <lodikhan2004@gmail.com>
2024-12-09 21:31:34 +0530

    Add solution for basic concepts of oop's

---
+++ OOPS/OOP.java
@@ -0,0 +1,67 @@
+package OOPS;
+
+public class OOP{
+    public static void main(String arg[]){
+        // created pen object called p1
+        Pen p1 = new Pen(); 
+        p1.setColor("BLUE");
+        System.out.println(p1.color);
+        //p1.setTip(5);
+        System.out.println(p1.tip = 5);
+
+        //created Student object called s1
+        Student s1 = new Student();
+        double res=s1.percentage =85;
+        System.out.println(res);
+        
+        //creted BankAccount obj called acc
+        BankAccount acc = new BankAccount();
+        acc.username="afzal";
+        System.out.println(acc.username);
+       
+       //acc.password="lodi@123"; => cannot be accessed
+       acc.setPassword("lodi@123");// can be modified
+       System.out.println(acc.setPassword("lodi@123"));
+
+
+
+    }   
+}
+
+class BankAccount{
+    public String username; //can be accessed
+    private String password; //cannot be accessed
+
+    public String setPassword(String pwd){ //can be accessed
+        password = pwd;
+    
+    return password;
+    }
+    
+
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    float percentage;
+    
+    void calcpercentage(int math ,int phy, int chem){
+        percentage = (math + phy + chem)/3;
+    }
+}
+
+
+
