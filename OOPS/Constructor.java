3c898484cbc982a10601cb05debe8deb5713a06d
Lodikhan2004 <lodikhan2004@gmail.com>
2025-11-05 20:29:58 +0530

    added constructor

---
+++ OOPS/Constructor.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student("afzal");
+        System.out.println(s1.name);
+        Student s2 = new Student("venkat");
+        System.out.println(s2.name);
+     
+        
+    }
+    
+}
+class Student{
+    String name;
+    int roll;
+
+    Student(String name){
+        System.out.println("constructor is called when object is created");
+        this.name = name;
+    }
+}
