c3bd7e9adcea47bc761a1b7804504a6a044257de
Lodikhan2004 <lodikhan2004@gmail.com>
2025-11-05 20:27:29 +0530

    learned classes and objects basics

--- OOPS/OOP.java
+++ OOPS/OOP.java
@@ -1,41 +1,23 @@
 package OOPS;
 
 public class OOP{
-    public static void main(String arg[]){
-        // created pen object called p1
-        Pen p1 = new Pen(); 
-        p1.setColor("BLUE");
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // crated a pen object
+        p1.setColor("blue");
         System.out.println(p1.color);
-        //p1.setTip(5);
-        System.out.println(p1.tip = 5);
 
-        //created Student object called s1
-        Student s1 = new Student();
-        double res=s1.percentage =85;
-        System.out.println(res);
-        
-        //creted BankAccount obj called acc
-        BankAccount acc = new BankAccount();
-        acc.username="afzal";
-        System.out.println(acc.username);
-       
-       //acc.password="lodi@123"; => cannot be accessed
-       acc.setPassword("lodi@123");// can be modified
+        p1.setTip(5);
+        System.out.println(p1.tip);
 
-    }   
-}
+        p1.color = "yellow";
+        System.out.println(p1.color);
 
-class BankAccount{
-    public String username; //can be accessed
-    private String password; //cannot be accessed
-
-    public void setPassword(String pwd){ //can be accessed
-        password = pwd;
+        BankAccount myAcc = new BankAccount();
+        myAcc.name = "adf";
+        myAcc.setPassword(646);
     }
-    
 
 }
-
 class Pen {
     String color;
     int tip;
@@ -43,20 +25,25 @@ class Pen {
     void setColor(String newColor){
         color = newColor;
     }
-
     void setTip(int newTip){
         tip = newTip;
     }
+
 }
 
-class Student {
-    String name;
-    float percentage;
-    
-    void calcpercentage(int math ,int phy, int chem){
-        percentage = (math + phy + chem)/3;
+class BankAccount{
+    public String name;
+    private int password;
+
+    public void setPassword(int newpassword){
+        password = newpassword;
     }
+
 }
+
+  
+
+
 
 
 
