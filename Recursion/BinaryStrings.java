ad3f4395a639c08930185134e0b4ac1da954f43a
Lodikhan2004 <lodikhan2004@gmail.com>
2025-11-14 12:29:58 +0530

    Added BinaryString Problem

---
+++ Recursion/BinaryStrings.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class BinaryStrings {
+    public static void BinStr(int n,String str, int lastPlace){
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+        //Kaam
+        BinStr(n-1, str+"0", 0);
+        if(lastPlace == 0){
+            BinStr(n-1, str+"1", 1);
+        }
+       
+    }
+    public static void main(String[] args) {
+        BinStr(3, new String(""), 0);
+       
+        
+    }
+    
+}
