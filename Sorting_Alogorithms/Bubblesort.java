c5ef61b472869f33450b461ce1811ee99370674c
Lodikhan2004 <lodikhan2004@gmail.com>
2025-11-19 19:46:25 +0530

    Added Optimized Bubble sort

--- Sorting_Alogorithms/Bubblesort.java
+++ Sorting_Alogorithms/Bubblesort.java
@@ -6,12 +6,17 @@ public class Bubblesort{
     public static void bubblesort(int arr[] ){
         int n= arr.length;
         for(int i=0 ; i<=n-2;i++){     //n-2 --> not compare last element 
+            boolean swapped = false;
             for(int j=0 ; j<=n-2-i; j++){   //the comparison range decreases because the largest elements are already sorted.
                 if(arr[j] > arr[j+1]){
                     int temp =arr [j];
                     arr[j] = arr[j+1];
                     arr[j+1]= temp;
+                    swapped = true;
                 }
+            }
+            if(swapped == false){ // if no swapp's happend then it is sorted come out of the loop
+                break;
             }
         } 
         
@@ -34,4 +39,7 @@ public class Bubblesort{
         sc.close();
     }
 }
-//TIME COMPLEXITY==>O(n**2)
+//TIME COMPLEXITY==>O(n**2) for normal code 
+/*Time complexity for optimized code :
+Worst case : O(n**2)
+Best case : O(n) */
