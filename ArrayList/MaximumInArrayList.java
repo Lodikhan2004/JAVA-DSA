c0da8d7e565b4e349cf1232890431c6c9ee12507
Lodikhan2004 <lodikhan2004@gmail.com>
2025-11-28 20:19:16 +0530

    Added Maximum in array

---
+++ ArrayList/MaximumInArrayList.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class MaximumInArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int  max = Integer.MIN_VALUE; //-infinity
+        for(int i=0; i< list.size(); i++){
+            // if(list.get(i) > max){
+            //     max = list.get(i);
+            // }
+            max = Math.max(max,list.get(i));
+        }
+        System.out.println("Maximum elem in ArrayList is :" + max);
+      
+    }
+    
+}
