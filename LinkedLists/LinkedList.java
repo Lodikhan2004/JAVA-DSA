7778cb2bc26fe9f2f5c2dcd3602e63c97434c96a
Lodikhan2004 <lodikhan2004@gmail.com>
2025-12-09 18:57:27 +0530

    Added How to create a node

---
+++ LinkedLists/LinkedList.java
@@ -0,0 +1,29 @@
+package LinkedLists;
+
+public class LinkedList {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        Node newNode = new Node(1);
+        Node n2 = new Node(2);
+        newNode.next = n2;
+        ll.head = newNode;
+        ll.tail = n2;
+        System.out.println(head.data);
+        System.out.println(tail.data);
+
+    }
+    
+}
