31225fef409eae89765d0dd9104f418d5d179f04
Lodikhan2004 <lodikhan2004@gmail.com>
2026-01-01 23:20:53 +0530

    Added Post order problem

---
+++ BinaryTrees/PostOrder.java
@@ -0,0 +1,47 @@
+package BinaryTrees;
+
+public class PostOrder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static int idx = -1;
+    public static Node buildTree(int nodes[]){
+        idx++;
+        if(nodes[idx] == -1){
+            return null;
+        }
+        //create a node
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+
+        return newNode; // return root node
+
+    }
+    //Post Order
+    public static void postOrder(Node root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+    
+    
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = buildTree(nodes);
+        postOrder(root);
+       
+    }
+    
+}
